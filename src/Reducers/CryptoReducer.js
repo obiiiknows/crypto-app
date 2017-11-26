de6c7fc334e6bf801a15bd899b9600177e5bd841
obi <sam.nnodim@gmail.com>
2017-11-25 22:49:33 -0800

    working redux requests to coinmarketcap (see the console). this tutorial hella litty

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,48 @@
-export default function(state = [], action) {
-  return state
+// Action Types
+import {
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_DATA_SUCCESS,
+  FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
+
+// Initial State
+const initialState = {
+  isFetching: false,
+  data: null,
+  hasError: false,
+  errorMessage: null
+}
+
+export default function(state = initialState, action) {
+  
+  switch(action.type) {
+
+    case FETCHING_COIN_DATA:
+        return Object.assign(state, {
+          isFetching: true,
+          data: null,
+          hasError: false,
+          errorMessage: null
+        });
+    
+      case FETCHING_COIN_DATA_SUCCESS:
+          return Object.assign(state, {
+            isFetching: true,
+            data: action.payload,
+            hasError: false,
+            errorMessage: null
+          });
+
+      case FETCHING_COIN_DATA_FAIL:
+          return Object.assign(state, {
+            isFetching: true,
+            data: null,
+            hasError: false,
+            errorMessage: action.payload
+          });
+
+    default:
+        return state;
+
+  }
 }
--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 
+import FetchCoinData from './../Actions/FetchCoinData';
+
 class CryptoContainer extends Component {
+
+componentDidMount() {
+  this.props.FetchCoinData();
+}
+
   render() {
     return (
       <View>
@@ -18,4 +25,4 @@ function mapStatetoProps(state) {
   }
 }
 
-export default connect(mapStatetoProps)(CryptoContainer)
+export default connect(mapStatetoProps, { FetchCoinData })(CryptoContainer)
